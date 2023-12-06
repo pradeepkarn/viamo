fd46ee757322d2aca3fac3f427ddcdf211c04e9b
pradeepkarn <mail2pkarn@gmail.com>
2023-12-06 12:36:26 +0530

    json data

--- functions.php
+++ functions.php
@@ -2301,7 +2301,7 @@ function getTextFromCode($code, $arr)
 function database_read($orderId)
 {
     $orderId = intval($orderId);
-    $database = dirname(__FILE__) . "/database/order-{$orderId}.txt";
+    $database = dirname(__FILE__) . "/jsondata/order-{$orderId}.txt";
 
     $status = @file_get_contents($database);
 
@@ -2311,7 +2311,7 @@ function database_read($orderId)
 function database_write($orderId, $status)
 {
     $orderId = intval($orderId);
-    $database = dirname(__FILE__) . "/database/order-{$orderId}.txt";
+    $database = dirname(__FILE__) . "/jsondata/order-{$orderId}.txt";
 
     file_put_contents($database, $status);
 }
