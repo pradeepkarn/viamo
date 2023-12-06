941fc2f4dc0bc9f99e4f184b910bcbb0b9a5afd6
pradeepkarn <mail2pkarn@gmail.com>
2023-12-06 18:28:28 +0530

    myorders

--- apps/view/inc/sidebar.php
+++ apps/view/inc/sidebar.php
@@ -96,7 +96,7 @@
                         </nav>
                     </div>
                     <?php
-                    if (is_superuser()) {
+                    if (authenticate()) {
                     ?>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                             <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
